69e4fc87bf2b6b85a509aeee6c9ec4d17fd69621
rgolusuTR <raju.golusu@thomsonreuters.com>
2026-02-23 23:17:19 +0530

    feat: Add GitHub Pages deployment with GitHub Actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/image-optimization-main',
+  assetPrefix: '/image-optimization-main/',
 };
 
 export default nextConfig;
